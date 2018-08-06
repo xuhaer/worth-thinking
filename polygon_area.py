57a4c17c56aa76baa749ed808b0a09efd6ec60a5
hardly <trigal@qq.com>
2018-08-06 23:01:08 +0800

    polygon_area

---
+++ polygon_area.py
@@ -0,0 +1,14 @@
+'''
+P是多边形端点坐标的列表。
+给定端点坐标，该函数可以计算任意简单多边形的面积。
+
+'''
+def polygon_area(P):
+    n = len(P)
+    P.append(P[0])
+    S = 0
+    for i in range(0, n):
+        x1,y1 = P[i]
+        x2,y2 = P[i + 1]
+        S = S + (x1 + x2) * (y2 - y1)
+    return 0.5 * abs(S)
